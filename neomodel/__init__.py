5f1be9e4cce41a537f67eb63541e2b08e74ed9f2
mar-chi-pan <mar.polatoglou@gmail.com>
2012-09-21 10:43:40 +0200

    Update neomodel/__init__.py
    add EITHER to relationships

--- neomodel/__init__.py
+++ neomodel/__init__.py
@@ -1,4 +1,4 @@
 from .core import StructuredNode, StringProperty, IntegerProperty, FloatProperty, BoolProperty
-from .relationship import OUTGOING, INCOMING, NotConnected
+from .relationship import OUTGOING, INCOMING, EITHER, NotConnected
 from .cardinality import (AttemptedCardinalityViolation,
         CardinalityViolation, ZeroOrMore, OneOrMore, ZeroOrOne, One)
