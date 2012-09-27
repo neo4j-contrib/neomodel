f91e85aa7be9bf0313dcc4f095343d5446860627
Robin Edwards <robin.ge@gmail.com>
2012-09-27 10:11:28 +0100

    Export direction constants

--- neomodel/__init__.py
+++ neomodel/__init__.py
@@ -1,5 +1,5 @@
 from .core import StructuredNode, ReadOnlyNode, ReadOnlyError
-from .relationship import NotConnected
+from .relationship import NotConnected, OUTGOING, INCOMING, EITHER
 from .cardinality import (AttemptedCardinalityViolation,
         CardinalityViolation, ZeroOrMore, OneOrMore, ZeroOrOne, One)
 from .properties import StringProperty, IntegerProperty, FloatProperty, BoolProperty
