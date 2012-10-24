277d6069e6f55ce8e7a84d51e303e8cb05cda6ba
Robin Edwards <robin.ge@gmail.com>
2012-10-24 15:36:32 +0100

    Cleanup

--- neomodel/__init__.py
+++ neomodel/__init__.py
@@ -1,7 +1,9 @@
-from .core import StructuredNode, ReadOnlyNode, ReadOnlyError, DoesNotExist, UniqueProperty, CypherException
-from .relationship import NotConnected, OUTGOING, INCOMING, EITHER, RelationshipTo, RelationshipFrom, Relationship
+from .core import (StructuredNode, ReadOnlyNode, ReadOnlyError, DoesNotExist,
+        UniqueProperty, CypherException)
+from .relationship import (NotConnected, OUTGOING, INCOMING, EITHER, RelationshipTo,
+        RelationshipFrom, Relationship)
 from .cardinality import (AttemptedCardinalityViolation,
         CardinalityViolation, ZeroOrMore, OneOrMore, ZeroOrOne, One)
-from .properties import StringProperty, IntegerProperty, FloatProperty, BooleanProperty,\
-    DatetimeProperty, DateProperty
+from .properties import (StringProperty, IntegerProperty,
+        FloatProperty, BooleanProperty, DatetimeProperty, DateProperty)
 from .exception import InflateError, DeflateError
