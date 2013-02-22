d1d449d9083fde1a3cab393cab5be6dab00c0e15
Robin Edwards <robin.ge@gmail.com>
2013-02-22 14:55:20 +0000

    Improve tagging

--- setup.py
+++ setup.py
@@ -14,9 +14,14 @@ setup(
     tests_require=['nose==1.1.2'],
     install_requires=['lucene-querybuilder==0.1.6', 'py2neo==1.4.6', 'pytz==2012g'],
     classifiers=[
-        'Development Status :: 5 - Prodction/Stable',
+        "Development Status :: 5 - Production/Stable",
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Database",
         ])
