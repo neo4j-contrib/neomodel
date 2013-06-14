1c78fc1c52b3b91d9d572791221ecb65cc4db348
Robin Edwards <robin.ge@gmail.com>
2013-06-14 22:19:47 +0100

    Avoid deprecated method

--- neomodel/cardinality.py
+++ neomodel/cardinality.py
@@ -66,7 +66,7 @@ class One(RelationshipManager):
     def connect(self, obj):
         if not self.origin.__node__:
             raise Exception("Node has not been saved cannot connect!")
-        if self.origin.__node__.has_relationship(self.direction, self.relation_type):
+        if len(self):
             raise AttemptedCardinalityViolation("Node already has one relationship")
         else:
             return super(One, self).connect(obj)
