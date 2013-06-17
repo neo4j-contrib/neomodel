9218afd55b23b6461f243e9a1e15b5ffb68faff2
Robin Edwards <robin.ge@gmail.com>
2013-06-17 17:56:09 +0100

    Python 3 fix for func_name

--- neomodel/signals.py
+++ neomodel/signals.py
@@ -20,8 +20,9 @@ def exec_hook(hook_name, self, *args, **kwargs):
 
 def hooks(fn):
     def hooked(self, *args, **kwargs):
-        exec_hook('pre_' + fn.func_name, self, *args, **kwargs)
+        fn_name = fn.func_name if hasattr(fn, 'func_name') else fn.__name__
+        exec_hook('pre_' + fn_name, self, *args, **kwargs)
         val = fn(self, *args, **kwargs)
-        exec_hook('post_' + fn.func_name, self, *args, **kwargs)
+        exec_hook('post_' + fn_name, self, *args, **kwargs)
         return val
     return hooked
