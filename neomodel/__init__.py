87cff094c9d7fb7c165fc3df8ae676fa89e9729a
Panos <jiguromusha7@gmail.com>
2013-06-17 13:08:38 +0100

    Include JSONProperty in imports

--- neomodel/__init__.py
+++ neomodel/__init__.py
@@ -1,9 +1,10 @@
 from .core import *
-from .relationship import (NotConnected, OUTGOING, INCOMING, EITHER, RelationshipTo,
-        RelationshipFrom, Relationship)
+from .relationship import (NotConnected, OUTGOING, INCOMING, EITHER,
+        RelationshipTo, RelationshipFrom, Relationship)
 from .cardinality import (AttemptedCardinalityViolation,
         CardinalityViolation, ZeroOrMore, OneOrMore, ZeroOrOne, One)
 from .properties import (StringProperty, IntegerProperty, AliasProperty,
-        FloatProperty, BooleanProperty, DateTimeProperty, DateProperty)
+        FloatProperty, BooleanProperty, DateTimeProperty, DateProperty,
+        JSONProperty)
 from .exception import InflateError, DeflateError, UniqueProperty
 from .signals import SIGNAL_SUPPORT
