91cf142acc5ef67b82b3b146be4a707e5b2b02a8
Panos <jiguromusha7@gmail.com>
2013-07-23 14:34:13 +0100

    Some more docs

--- test/test_relative_relationships.py
+++ test/test_relative_relationships.py
@@ -4,6 +4,7 @@ from .test_relationships import Country
 
 class Cat(StructuredNode):
     name = StringProperty()
+    # Relationship is defined using a relative class path
     is_from = RelationshipTo('.test_relationships.Country', 'IS_FROM')
 
 
@@ -14,5 +15,7 @@ def test_relative_relationship():
     c = Country(code='MG').save()
     assert c
 
+    # connecting an instance of the class defined above
+    # the next statement will fail if there's a type mismatch
     a.is_from.connect(c)
     assert a.is_from.is_connected(c)
