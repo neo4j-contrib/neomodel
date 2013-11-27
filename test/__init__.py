23c07b8bbddd2af614dd75dcac7b85ca5bad07fc
Robin Edwards <robin.ge@gmail.com>
2013-11-27 12:33:02 +0000

    Print out neo4j version

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,6 @@
+from __future__ import print_function
 import warnings
-warnings.simplefilter('default')
 from neomodel.core import connection
+warnings.simplefilter('default')
 connection().clear()
+print("neo4j version: ", *connection().neo4j_version)
