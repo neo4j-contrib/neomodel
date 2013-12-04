ac86d91ac1d825e19b155875f2ffcd9cc5cf61b0
Robin Edwards <robin.ge@gmail.com>
2013-12-04 17:14:10 +0000

    Fix index conflict issue with 1.9
    py2neo no longer raises exception on index conflict

--- neomodel/util.py
+++ neomodel/util.py
@@ -16,22 +16,27 @@ class CustomBatch(neo4j.WriteBatch):
         self.node = node
 
     def submit(self):
-        try:
-            responses = self._execute()
-            batch_responses = [neo4j.BatchResponse(r) for r in responses.json]
-            if self._graph_db.neo4j_version < (1, 9):
-                self._check_for_conflicts(responses, batch_responses, self._requests)
-        except neo4j.BatchError as r:
-            key = self._requests[r.id].body['key']
-            value = self._requests[r.id].body['value']
-            raise UniqueProperty(key, value, self.index_name, self.node)
+        responses = self._execute()
+        batch_responses = [neo4j.BatchResponse(r) for r in responses.json]
+        if self._graph_db.neo4j_version < (1, 9):
+            self._legacy_check_for_conflicts(responses, batch_responses, self._requests)
         else:
-            try:
-                return [r.hydrated for r in batch_responses]
-            finally:
-                responses.close()
+            self._check_for_conflicts(responses, batch_responses, self._requests)
+
+        try:
+            return [r.hydrated for r in batch_responses]
+        finally:
+            responses.close()
 
     def _check_for_conflicts(self, responses, batch_responses, requests):
+        for i, r in enumerate(batch_responses):
+            if r.status_code == 409:
+                responses.close()
+                raise UniqueProperty(
+                        requests[i].body['key'], requests[i].body['key'],
+                        self.index_name, self.node)
+
+    def _legacy_check_for_conflicts(self, responses, batch_responses, requests):
         for i, r in enumerate(batch_responses):
             if r.status_code == 200:
                 responses.close()
@@ -41,6 +46,10 @@ class CustomBatch(neo4j.WriteBatch):
 
 
 def _legacy_conflict_check(cls, node, props):
+    """
+    prior to the introduction of create_or_fail in 1.9 we check to see if the key
+    exists in the index before executing the batch.
+    """
     for key, value in props.items():
         if key in cls._class_properties() and cls.get_property(key).unique_index:
             results = cls.index.__index__.get(key, value)
