b8cf9a22d52b590931839ad44edbef67aa62a7b4
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2014-02-25 16:55:35 -0500

    This adds supoports hierachy in relationships definition validating agains classes and labels

--- neomodel/relationship_manager.py
+++ neomodel/relationship_manager.py
@@ -101,7 +101,7 @@ class RelationshipManager(object):
     def _check_node(self, obj):
         """check for valid node i.e correct class and is saved"""
         for label, cls in self.label_map.items():
-            if obj.__class__ is cls:
+            if issubclass(object.__class__, cls):
                 if not hasattr(obj, '_id'):
                     raise ValueError("Can't preform operation on unsaved node " + repr(obj))
                 return
