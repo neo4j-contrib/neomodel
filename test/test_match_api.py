56fae14796ff7af8ce676ee56aa51fdc8bf9d0dc
Panos <jiguromusha7@gmail.com>
2014-03-18 17:32:45 +0000

    .build_ast() does not return the object, need to call it separately

--- test/test_match_api.py
+++ test/test_match_api.py
@@ -22,23 +22,27 @@ class Coffee(StructuredNode):
 
 def test_filter_exclude_via_labels():
     node_set = NodeSet(Coffee)
-    qb = QueryBuilder(node_set).build_ast()
+    qb = QueryBuilder(node_set)
+    qb.build_ast()
     assert '(coffee:Coffee)' in qb._ast['match']
 
     # with filter and exclude
     node_set = node_set.filter(price__gt=2).exclude(price__gt=6, name='Java')
-    qb = QueryBuilder(node_set).build_ast()
+    qb = QueryBuilder(node_set)
+    qb.build_ast()
     assert '(coffee:Coffee)' in qb._ast['match']
     assert 'NOT' in qb._ast['where'][1]
 
 
 def test_simple_has_via_label():
     ns = NodeSet(Coffee).has(suppliers=True)
-    qb = QueryBuilder(ns).build_ast()
+    qb = QueryBuilder(ns)
+    qb.build_ast()
     assert 'SUPPLIES' in qb._ast['match'][1]
 
     ns = NodeSet(Coffee).has(suppliers=False)
-    qb = QueryBuilder(ns).build_ast()
+    qb = QueryBuilder(ns)
+    qb.build_ast()
     assert 'NOT' in qb._ast['where'][0]
 
 
@@ -48,5 +52,6 @@ def test_simple_traverse():
         key='suppliers',
         definition=Coffee.suppliers.definition).match(since__lt=datetime.now())
 
-    qb = QueryBuilder(NodeSet(source=traversal)).build_ast()
+    qb = QueryBuilder(NodeSet(source=traversal))
+    qb.build_ast()
     print repr(qb._ast)
