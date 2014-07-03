7b6615f64426d81b9b7b6a62b8efbc8e26b771a8
Robin Edwards <robin.ge@gmail.com>
2014-07-03 16:46:17 +0100

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neomodel',
-    version='0.4.0',
+    version='0.4.1',
     description='An object mapper for the neo4j graph database.',
     long_description=open('README.rst').read(),
     author='Robin Edwards',
