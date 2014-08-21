3d63df5866ef1986a015815991441d76c79f1560
Robin Edwards <robin.ge@gmail.com>
2014-08-21 16:51:49 +0100

    Add python versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neomodel',
-    version='1.0.0',
+    version='1.0.1',
     description='An object mapper for the neo4j graph database.',
     long_description=open('README.rst').read(),
     author='Robin Edwards',
