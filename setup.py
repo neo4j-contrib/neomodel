d092318eaa4dd40d454df41d6f913d6f5ec658e2
Robin Edwards <robin.ge@gmail.com>
2014-10-21 10:58:38 -0700

    Fix package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name=__package__,
+    name='neomodel',
     version='1.0.2',
     description='An object mapper for the neo4j graph database.',
     long_description=open('README.rst').read(),
