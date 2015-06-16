11d36d81b45e410324750e5c5f71ba0fa400f156
Devon Bleibtrey <bleib1dj@gmail.com>
2015-06-16 19:06:22 -0400

    Reverting py2neo 2.0.8 change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     keywords='graph neo4j py2neo ORM',
     tests_require=['nose==1.1.2'],
     test_suite='nose.collector',
-    install_requires=['py2neo==2.0.8', 'pytz>=2015,<2016'],
+    install_requires=['py2neo==2.0.7', 'pytz>=2015,<2016'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         'Intended Audience :: Developers',
