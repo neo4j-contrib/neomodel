cef65dfe1813948bbc17e43472d20d3247f73a87
Frank Sachsenheim <funkyfuture@riseup.net>
2017-12-10 21:18:13 +0100

    Adds support for neo4j 3.3

--- tests-with-docker-compose.sh
+++ tests-with-docker-compose.sh
@@ -41,7 +41,7 @@ clean () {
 }
 
 
-for NEO4J_VERSION in 3.0 3.1 3.2; do
+for NEO4J_VERSION in 3.0 3.1 3.2 3.3; do
     for PYTHON_VERSION in 2.7 3.3 3.4 3.5 3.6; do
         write_compose_file
         docker-compose up -d neo4j
