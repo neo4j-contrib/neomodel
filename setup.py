1a06a75f8da71e4acfc9f5011eead288e91508c9
Jorge Valhondo Rama <jvalhondo@users.noreply.github.com>
2019-10-29 00:04:26 +0100

    remove python 2.7 classifier

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
