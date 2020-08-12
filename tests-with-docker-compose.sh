d0809bd7f10c62df77f3ec58997bcd1e8c9ba055
Takahashi Akira <a-takahashi@iij.ad.jp>
2020-08-12 10:46:09 +0900

    support Neo4j 3.4, 3.5 clearly

--- tests-with-docker-compose.sh
+++ tests-with-docker-compose.sh
@@ -47,7 +47,7 @@ for dir in neomodel test; do
     find ${dir} -name __pycache__ -exec rm -Rf {} \;
 done
 
-for NEO4J_VERSION in 3.0 3.1 3.2 3.3; do
+for NEO4J_VERSION in 3.0 3.1 3.2 3.3 3.4 3.5; do
     for PYTHON_VERSION in 2.7 3.4 3.5 3.6; do
         write_compose_file
         docker-compose up -d neo4j
