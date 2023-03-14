411049c53ee24a0ca57bacf628e016ebf17dd420
Marius Conjeaud <rmcj@novonordisk.com>
2023-03-14 12:35:37 +0100

    Add Python 3.11 to test matrix

--- tests-with-docker-compose.sh
+++ tests-with-docker-compose.sh
@@ -14,7 +14,7 @@ for dir in neomodel test; do
     find ${dir} -name __pycache__ -exec rm -Rf {} \;
 done
 : "${NEO4J_VERSIONS:=5.4 4.4}"
-: "${PYTHON_VERSIONS:=3.10 3.9 3.8 3.7}"
+: "${PYTHON_VERSIONS:=3.11 3.10 3.9 3.8 3.7}"
 for NEO4J_VERSION in ${NEO4J_VERSIONS}; do
     for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
         export NEO4J_VERSION
