641aed5f43ea00210957cfe3d92fbcfae8f59b34
Marius Conjeaud <rmcj@novonordisk.com>
2023-08-02 14:11:29 +0200

    Fix pytest skipif - Missing reason info

--- test/test_driver_options.py
+++ test/test_driver_options.py
@@ -5,21 +5,27 @@ from pytest import raises
 from neomodel import db
 
 
-@pytest.mark.skipif(db.database_edition != "enterprise")
+@pytest.mark.skipif(
+    db.database_edition != "enterprise", reason="Skipping test for community edition"
+)
 @db.impersonate(user="troygreene")
 def test_impersonate():
     results, _ = db.cypher_query("RETURN 'Doo Wacko !'")
     assert results[0][0] == "Doo Wacko !"
 
 
-@pytest.mark.skipif(db.database_edition != "enterprise")
+@pytest.mark.skipif(
+    db.database_edition != "enterprise", reason="Skipping test for community edition"
+)
 def test_impersonate_unauthorized():
     with db.impersonate(user="unknownuser"):
         with raises(ClientError):
             _ = db.cypher_query("RETURN 'Gabagool'")
 
 
-@pytest.mark.skipif(db.database_edition != "enterprise")
+@pytest.mark.skipif(
+    db.database_edition != "enterprise", reason="Skipping test for community edition"
+)
 def test_impersonate_multiple_transactions():
     with db.impersonate(user="troygreene"):
         with db.transaction:
