fadeaec65e7b32fad1f079ebb504e45156ce57a5
Marius Conjeaud <rmcj@novonordisk.com>
2023-10-30 16:20:58 +0100

    Ignore empty lines in console output and test/generated file

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -146,24 +146,26 @@ def test_neomodel_inspect_database():
         check=False,
     )
 
-    console_output = result.stdout
-    wrapped_console_output = console_output.split("\n")
+    wrapped_console_output = [
+        line for line in result.stdout.splitlines() if line.strip()
+    ]
     assert wrapped_console_output[0].startswith("Connecting to")
     # Check that all the expected lines are here
-    # with open("test/data/neomodel_inspect_database_output.txt", "r") as f:
-    #     for line in f.read().split("\n"):
-    #         # The neomodel components import order might differ
-    #         # So let's check that every import that should be added is added, regardless of order
-    #         if line.startswith("from neomodel import"):
-    #             parsed_imports = line.replace("from neomodel import ", "").split(", ")
-    #             expected_imports = (
-    #                 wrapped_console_output[1]
-    #                 .replace("from neomodel import ", "")
-    #                 .split(", ")
-    #             )
-    #             assert set(parsed_imports) == set(expected_imports)
-    #         else:
-    #             assert line in wrapped_console_output
+    with open("test/data/neomodel_inspect_database_output.txt", "r") as f:
+        wrapped_test_file = [line for line in f.read().split("\n") if line.strip()]
+        for line in wrapped_test_file:
+            # The neomodel components import order might differ
+            # So let's check that every import that should be added is added, regardless of order
+            if line.startswith("from neomodel import"):
+                parsed_imports = line.replace("from neomodel import ", "").split(", ")
+                expected_imports = (
+                    wrapped_console_output[1]
+                    .replace("from neomodel import ", "")
+                    .split(", ")
+                )
+                assert set(parsed_imports) == set(expected_imports)
+            else:
+                assert line in wrapped_console_output
 
     # Test the file output version of the script
     result = subprocess.run(
@@ -182,13 +184,14 @@ def test_neomodel_inspect_database():
     # Check that the file was written
     # And that the file output has the same content as the console output
     # Again, regardless of order and redundance
-    print(result.stdout)
-    wrapped_file_console_output = result.stdout.split("\n")
+    wrapped_file_console_output = [
+        line for line in result.stdout.splitlines() if line.strip()
+    ]
     assert wrapped_file_console_output[0].startswith("Connecting to")
     assert wrapped_file_console_output[1].startswith("Writing to")
     with open("test/data/neomodel_inspect_database_test_output.py", "r") as f:
-        # assert set(f.read().split("\n")) == set(wrapped_console_output[1:])
-        print(f.read())
+        wrapped_output_file = [line for line in f.read().split("\n") if line.strip()]
+        assert set(wrapped_output_file) == set(wrapped_console_output[1:])
 
     # Finally, delete the file created by the script
     subprocess.run(
