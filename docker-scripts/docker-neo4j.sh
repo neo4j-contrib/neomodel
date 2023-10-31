bcd84af81b54ce7205ddcfae803a7415f9b7e329
Marius Conjeaud <rmcj@novonordisk.com>
2023-10-31 13:59:13 +0100

    Try installating apoc 4.4 for Neo4J 4.4

--- docker-scripts/docker-neo4j.sh
+++ docker-scripts/docker-neo4j.sh
@@ -1,8 +1,19 @@
-docker run \
-    --name neo4j \
-    -p7474:7474 -p7687:7687 \
-    -d \
-    --env NEO4J_AUTH=neo4j/foobarbaz \
-    --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --env NEO4JLABS_PLUGINS='["apoc"]' \
-    neo4j:$1
+if [[ "$1" == 4.4* ]]; then
+    docker run \
+        --name neo4j \
+        -p7474:7474 -p7687:7687 \
+        -d \
+        --env NEO4J_AUTH=neo4j/foobarbaz \
+        --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
+        --env NEO4JLABS_PLUGINS='["apoc-4.4.0.23-all"]' \
+        neo4j:$1
+else
+    docker run \
+        --name neo4j \
+        -p7474:7474 -p7687:7687 \
+        -d \
+        --env NEO4J_AUTH=neo4j/foobarbaz \
+        --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
+        --env NEO4JLABS_PLUGINS='["apoc"]' \        neo4j:$1
+
+fi
