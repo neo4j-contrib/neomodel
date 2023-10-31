14ccb9cc2a181c1989b5096238d6592f9b03bd05
Marius Conjeaud <rmcj@novonordisk.com>
2023-10-31 14:04:08 +0100

    Fix docker script

--- docker-scripts/docker-neo4j.sh
+++ docker-scripts/docker-neo4j.sh
@@ -1,11 +1,11 @@
-if [[ "$1" == 4.4* ]]; then
+if [ $1 = "4.4*" ]; then
     docker run \
         --name neo4j \
         -p7474:7474 -p7687:7687 \
         -d \
         --env NEO4J_AUTH=neo4j/foobarbaz \
         --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-        --env NEO4JLABS_PLUGINS='["apoc-4.4.0.23-all"]' \
+        --env NEO4JLABS_PLUGINS=["apoc-4.4.0.23-all"] \
         neo4j:$1
 else
     docker run \
@@ -14,6 +14,7 @@ else
         -d \
         --env NEO4J_AUTH=neo4j/foobarbaz \
         --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-        --env NEO4JLABS_PLUGINS='["apoc"]' \        neo4j:$1
+        --env NEO4JLABS_PLUGINS=["apoc"] \
+        neo4j:$1
 
 fi
