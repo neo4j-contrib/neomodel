9cb0ac167b52cf6ffbccc44bbab1c9ac3ca65a28
Marius Conjeaud <rmcj@novonordisk.com>
2023-12-08 12:14:42 +0100

    Fix version tag to integer and add tests

--- test/test_dbms_awareness.py
+++ test/test_dbms_awareness.py
@@ -1,6 +1,7 @@
 from pytest import mark
 
 from neomodel import db
+from neomodel.util import version_tag_to_integer
 
 
 @mark.skipif(
@@ -9,6 +10,7 @@ from neomodel import db
 def test_version_awareness():
     assert db.database_version == "5.7.0"
     assert db.version_is_higher_than("5.7")
+    assert db.version_is_higher_than("5.6.0")
     assert db.version_is_higher_than("5")
     assert db.version_is_higher_than("4")
 
@@ -20,3 +22,11 @@ def test_edition_awareness():
         assert db.edition_is_enterprise()
     else:
         assert not db.edition_is_enterprise()
+
+
+def test_version_tag_to_integer():
+    assert version_tag_to_integer("5.7.1") == 50701
+    assert version_tag_to_integer("5.1") == 50100
+    assert version_tag_to_integer("5") == 50000
+    assert version_tag_to_integer("5.14.1") == 51401
+    assert version_tag_to_integer("5.14-aura") == 51400
