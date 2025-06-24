4d62cb9c301ce9ab598892ec36dd49e9f0a09a6d
greengori11a <jooapm29@gmail.com>
2025-06-24 16:43:44 +1000

    not anything genuine, starting to better understand the filter construction maybe?

--- neomodel/sync_/vectorfilter.py
+++ neomodel/sync_/vectorfilter.py
@@ -7,10 +7,9 @@ class VectorFilter():
     Represents a CALL db.index.vector.query* neo functions call within the OGM - to allow for a neomodel query method that isnt running the function within a query_cyphe call. 
 
     """
-    def __init__(self, topk: int, index_name: str, candidate_vector: List[float], score: str):
-
+    def __init__(self, topk: int, index_name: str, candidate_vector: List[float]):
+        # Not doing score thresholding because I dont know how to DJANGO filters.
         self.topk = topk
         self.index_name = index_name
         self.vector = candidate_vector
-        self.score = score # The score here is actually a django filtered string
 
