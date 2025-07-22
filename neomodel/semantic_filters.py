ba6a6691c3cdb508229c6c0ccd4131d4c31d94f1
greengori11a <jooapm29@gmail.com>
2025-07-22 12:24:28 +1000

    wrote doc string for VectorFilter Class

--- neomodel/semantic_filters.py
+++ neomodel/semantic_filters.py
@@ -4,7 +4,13 @@ class VectorFilter(object):
     """
     Represents a CALL db.index.vector.query* neo functions call within the OGM
     
-    The keyword argument for this in the filter function is: vector_filter (when we do the fulltextindex filter thing we should change this to be like index_filter or something so that we dont need to do this twice)
+    :param topk: The amount of objects returned by this vector filter. 
+    :type topk: int
+    :param vector_attribute_name: The property name for vector indexed property on the searched object.
+    :type vector_attribute_name: str
+    :param candidate_vector: The vector you are finding the nearest topk neighbours for. 
+    :type candidate_vector: list[float]
+
     """
     def __init__(self, topk: int, vector_attribute_name: str, candidate_vector: List[float]):
         self.topk = topk
