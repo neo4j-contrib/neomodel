b76dad06e039f63bd85ba7e59ad11db6712d5cb6
Marius Conjeaud <marius.conjeaud@outlook.com>
2025-09-24 18:25:40 +0200

    Revert "Merge remote-tracking branch 'origin/rc/6.0.0' into fix/888-async-database-transactions"
    This reverts commit 983def814524c38cb99d5b8d5a1a6c16753d89e3, reversing
    changes made to 3424ab23d027e059b728371f359c60ef4400be56.

--- neomodel/sync_/core.py
+++ neomodel/sync_/core.py
@@ -37,7 +37,12 @@ from neomodel.exceptions import (
 from neomodel.hooks import hooks
 from neomodel.properties import FulltextIndex, Property, VectorIndex
 from neomodel.sync_.property_manager import PropertyManager
-from neomodel.util import _UnsavedNode, classproperty, version_tag_to_integer
+from neomodel.util import (
+    _UnsavedNode,
+    classproperty,
+    deprecated,
+    version_tag_to_integer,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -777,9 +782,9 @@ class Database:
         """
         )
         if clear_constraints:
-            self.drop_constraints()
+            drop_constraints()
         if clear_indexes:
-            self.drop_indexes()
+            drop_indexes()
 
     def drop_constraints(
         self, quiet: bool = True, stdout: Optional[TextIO] = None
@@ -869,7 +874,7 @@ class Database:
         i = 0
         for cls in subsub(StructuredNode):
             stdout.write(f"Found {cls.__module__}.{cls.__name__}\n")
-            self.install_labels(cls, quiet=False, stdout=stdout)
+            install_labels(cls, quiet=False, stdout=stdout)
             i += 1
 
         if i:
@@ -1257,6 +1262,88 @@ class Database:
 
 # Create a singleton instance of the database object
 db = Database()
+
+
+# Deprecated methods
+def change_neo4j_password(db: Database, user: str, new_password: str) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.change_neo4j_password(user, new_password) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.change_neo4j_password(user, new_password)
+
+
+def clear_neo4j_database(
+    db: Database, clear_constraints: bool = False, clear_indexes: bool = False
+) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.clear_neo4j_database(clear_constraints, clear_indexes) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.clear_neo4j_database(clear_constraints, clear_indexes)
+
+
+def drop_constraints(quiet: bool = True, stdout: Optional[TextIO] = None) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.drop_constraints(quiet, stdout) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.drop_constraints(quiet, stdout)
+
+
+def drop_indexes(quiet: bool = True, stdout: Optional[TextIO] = None) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.drop_indexes(quiet, stdout) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.drop_indexes(quiet, stdout)
+
+
+def remove_all_labels(stdout: Optional[TextIO] = None) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.remove_all_labels(stdout) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.remove_all_labels(stdout)
+
+
+def install_labels(
+    cls: Any, quiet: bool = True, stdout: Optional[TextIO] = None
+) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.install_labels(cls, quiet, stdout) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.install_labels(cls, quiet, stdout)
+
+
+def install_all_labels(stdout: Optional[TextIO] = None) -> None:
+    deprecated(
+        """
+        This method has been moved to the Database singleton (db for sync, db for async).
+        Please use db.install_all_labels(stdout) instead.
+        This direct call will be removed in an upcoming version.
+        """
+    )
+    db.install_all_labels(stdout)
 
 
 class TransactionProxy:
